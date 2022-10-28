8c25c92d6813facc01c9a6a63bfcf8b9b64268e5
atarutin <andrey.tarutin@lykke-business.ch>
2022-10-28 11:32:37 +0300

    move get_github_compatible_repo_address to module

---
+++ github_utils.py
@@ -0,0 +1,18 @@
+"""
+GitHub utility helper function module.
+"""
+
+
+def get_github_compatible_repo_address(full_url: str) -> str:
+    """
+    Returns GitHub client compatible repository address.
+    The address returned can be used with official GitHub API client
+    to access the repository. It will be provided in the following 
+    format: <company>/<repository>
+    """
+    normalized = full_url.strip().lower()
+    if normalized.startswith('http'):
+        normalized = normalized.replace('http://', '').replace('https://', '')
+
+    chunks = normalized.split('/')
+    return '/'.join(chunks[1:])
--- release_manager.py
+++ release_manager.py
@@ -14,21 +14,7 @@ from core.nova_release import NovaRelease
 from core.nova_status import Status
 
 from jira_utils import *
-
-
-def get_github_compatible_repo_address(full_url: str) -> str:
-    """
-    Returns GitHub client compatible repository address.
-    The address returned can be used with official GitHub API client to access the repository.
-    It will be provided in the following format:
-        <company>/<repository>
-    """
-    normalized = full_url.strip().lower()
-    if normalized.startswith('http'):
-        normalized = normalized.replace('http://', '').replace('https://', '')
-
-    chunks = normalized.split('/')
-    return '/'.join(chunks[1:])
+from github_utils import *
 
 
 class ReleaseManager:
