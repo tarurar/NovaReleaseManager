c7586b85841f56e3c236d4a66e1b664e064f7d84
atarutin <andrey.tarutin@lykke-business.ch>
2023-08-23 15:24:12 +0300

    fully cover components and issues parser with unit tests

--- tests/test_jira_components_parser.py
+++ tests/test_jira_components_parser.py
@@ -6,12 +6,26 @@ from jira_utils import parse_jira_component
 from core.nova_component import NovaComponent, NovaEmptyComponent
 
 
-class MockComponent:
-    """ Mock Jira component """
+class MockedComponent:
+    """Mock Jira component"""
 
     def __init__(self, name, description):
         self.name = name
         self.description = description
+
+
+class MockedNamelessComponent:
+    """Mock nameless Jira component"""
+
+    def __init__(self, description):
+        self.description = description
+
+
+class MockedDescriptionlessComponent:
+    """Mock descriptionless Jira component"""
+
+    def __init__(self, name):
+        self.name = name
 
 
 def test_when_empty_component_provided():
@@ -20,33 +34,64 @@ def test_when_empty_component_provided():
 
 
 def test_when_component_has_no_description():
-    component = MockComponent('component name', None)
+    component = MockedComponent("component name", None)
     with pytest.raises(ValueError):
         parse_jira_component(component)
+
+
+def test_when_component_has_no_description_attr():
+    component = MockedDescriptionlessComponent("component name")
+    with pytest.raises(ValueError) as excinfo:
+        parse_jira_component(component)
+
+    assert "no description" in str(excinfo.value)
 
 
 def test_when_component_has_no_name():
-    component = MockComponent(None, 'component description')
+    component = MockedComponent(None, "component description")
     with pytest.raises(ValueError):
         parse_jira_component(component)
 
 
+def test_when_component_has_no_name_attr():
+    component = MockedNamelessComponent("component description")
+    with pytest.raises(ValueError) as excinfo:
+        parse_jira_component(component)
+
+    assert "no name" in str(excinfo.value)
+
+
 def test_when_component_has_invalid_description():
-    component = MockComponent('component name', 'invalid description')
+    component = MockedComponent("component name", "invalid description")
     with pytest.raises(ValueError):
         parse_jira_component(component)
 
 
 def test_when_component_is_valid():
-    component = MockComponent(
-        'component name', 'http://github.com/company/project')
+    component = MockedComponent(
+        "component name", "http://github.com/company/project"
+    )
     result = parse_jira_component(component)
     assert isinstance(result, NovaComponent)
 
 
-@pytest.mark.parametrize('component_name', ['n/a', 'N/A', 'N/A ', ' N/A', 'N/A  ', 'n/A', 'N/a', 'n/A ', ' N/a', 'N/a  '])
+@pytest.mark.parametrize(
+    "component_name",
+    [
+        "n/a",
+        "N/A",
+        "N/A ",
+        " N/A",
+        "N/A  ",
+        "n/A",
+        "N/a",
+        "n/A ",
+        " N/a",
+        "N/a  ",
+    ],
+)
 def test_when_component_is_named_na(component_name):
-    component = MockComponent(component_name, '')
+    component = MockedComponent(component_name, "")
     result = parse_jira_component(component)
     assert isinstance(result, NovaEmptyComponent)
     assert result.name == NovaEmptyComponent.default_component_name
--- tests/test_jira_issues_parser.py
+++ tests/test_jira_issues_parser.py
@@ -5,35 +5,44 @@ import pytest
 from jira_utils import parse_jira_issue, filter_jira_issue
 
 
-class MockComponent:
+class MockedComponent:
     """Mock Jira component"""
 
     def __init__(self, name):
         self.name = name
 
 
-class MockFields:
-    """ Mock Jira issue fields"""
+class MockedFields:
+    """Mock Jira issue fields"""
 
     def __init__(self, components: list, status_name: str, summary: str):
-        self.components = list(map(MockComponent, components))
-        self.status = MockStatus(status_name)
+        self.components = list(map(MockedComponent, components))
+        self.status = MockedStatus(status_name)
         self.summary = summary
 
 
-class MockStatus:
-    """ Mock Jira issue status """
+class MockedStatus:
+    """Mock Jira issue status"""
 
     def __init__(self, name: str):
         self.name = name
 
 
-class MockIssue:
-    """ Mock Jira issue """
+class MockedIssue:
+    """Mock Jira issue"""
 
-    def __init__(self, key: str, components: list, status_name: str, summary: str = ''):
-        self.fields = MockFields(components, status_name, summary)
+    def __init__(
+        self, key: str, components: list, status_name: str, summary: str = ""
+    ):
+        self.fields = MockedFields(components, status_name, summary)
         self.key = key
+
+
+class MockedKeylessIssue:
+    """Mock Jira issue without key"""
+
+    def __init__(self, components: list, status_name: str, summary: str = ""):
+        self.fields = MockedFields(components, status_name, summary)
 
 
 def test_when_empty_issue_provided():
@@ -41,56 +50,66 @@ def test_when_empty_issue_provided():
         parse_jira_issue(None)
 
 
-@pytest.mark.parametrize('issue_key', ['', None])
+@pytest.mark.parametrize("issue_key", ["", None])
 def test_when_issue_has_no_key(issue_key):
-    issue = MockIssue(issue_key, ['c1'], 'Selected For Release')
+    issue = MockedIssue(issue_key, ["c1"], "Selected For Release")
+    with pytest.raises(ValueError):
+        parse_jira_issue(issue)
+
+
+def test_when_issue_has_no_key_attr():
+    issue = MockedKeylessIssue(["c1"], "invalid status")
     with pytest.raises(ValueError):
         parse_jira_issue(issue)
 
 
 def test_when_issue_has_no_component():
-    issue = MockIssue('issue key', [], 'Selected For Release')
+    issue = MockedIssue("issue key", [], "Selected For Release")
     with pytest.raises(ValueError):
         parse_jira_issue(issue)
 
 
 def test_when_issue_has_more_than_one_component():
-    issue = MockIssue('issue key', ['c1', 'c2'], 'Selected For Release')
+    issue = MockedIssue("issue key", ["c1", "c2"], "Selected For Release")
     with pytest.raises(ValueError):
         parse_jira_issue(issue)
 
 
 def test_when_issue_has_invalid_status():
-    issue = MockIssue('issue key', ['c1'], 'invalid status')
+    issue = MockedIssue("issue key", ["c1"], "invalid status")
     with pytest.raises(ValueError):
         parse_jira_issue(issue)
 
 
-@pytest.mark.parametrize('issue_key,issue_status', [
-    ('issue key', 'Selected For Release')])
+@pytest.mark.parametrize(
+    "issue_key,issue_status", [("issue key", "Selected For Release")]
+)
 def test_when_issue_is_valid(issue_key, issue_status):
-    issue = MockIssue(issue_key, ['c1'], issue_status)
+    issue = MockedIssue(issue_key, ["c1"], issue_status)
     nova_task = parse_jira_issue(issue)
     assert nova_task is not None
 
 
-@pytest.mark.parametrize('jira_cmp_name,nova_cmp_name,expected', [
-    ('c1', 'c1', True),
-    ('c1', 'c2', False),
-    (' c1', 'c1 ', True),
-    ('C1', 'c1', True),
-    ('C1', 'C1', True),
-    (' C1', ' c1 ', True)])
+@pytest.mark.parametrize(
+    "jira_cmp_name,nova_cmp_name,expected",
+    [
+        ("c1", "c1", True),
+        ("c1", "c2", False),
+        (" c1", "c1 ", True),
+        ("C1", "c1", True),
+        ("C1", "C1", True),
+        (" C1", " c1 ", True),
+    ],
+)
 def test_if_jira_issue_belongs_to_component(
-        jira_cmp_name,
-        nova_cmp_name,
-        expected):
-    issue = MockIssue('issue key', [jira_cmp_name], 'Selected For Release')
+    jira_cmp_name, nova_cmp_name, expected
+):
+    issue = MockedIssue("issue key", [jira_cmp_name], "Selected For Release")
     result = filter_jira_issue(issue, nova_cmp_name)
     assert result == expected
 
 
 def test_when_jira_issue_has_no_component():
-    issue = MockIssue('issue key', [], 'Selected For Release')
+    issue = MockedIssue("issue key", [], "Selected For Release")
     with pytest.raises(ValueError):
-        filter_jira_issue(issue, 'c1')
+        filter_jira_issue(issue, "c1")
