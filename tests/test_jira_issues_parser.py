4e1d4a6264df1ecba5e3cfd42fcf7b8e364e8ba6
atarutin <andrey.tarutin@lykke-business.ch>
2023-08-23 22:52:55 +0300

    replace mocked dtos with named tuples

--- tests/test_jira_issues_parser.py
+++ tests/test_jira_issues_parser.py
@@ -1,48 +1,15 @@
 """
 Jira issues validation tests
 """
+from collections import namedtuple
 import pytest
 from jira_utils import parse_jira_issue, filter_jira_issue
 
-
-class MockedComponent:
-    """Mock Jira component"""
-
-    def __init__(self, name):
-        self.name = name
-
-
-class MockedFields:
-    """Mock Jira issue fields"""
-
-    def __init__(self, components: list, status_name: str, summary: str):
-        self.components = list(map(MockedComponent, components))
-        self.status = MockedStatus(status_name)
-        self.summary = summary
-
-
-class MockedStatus:
-    """Mock Jira issue status"""
-
-    def __init__(self, name: str):
-        self.name = name
-
-
-class MockedIssue:
-    """Mock Jira issue"""
-
-    def __init__(
-        self, key: str, components: list, status_name: str, summary: str = ""
-    ):
-        self.fields = MockedFields(components, status_name, summary)
-        self.key = key
-
-
-class MockedKeylessIssue:
-    """Mock Jira issue without key"""
-
-    def __init__(self, components: list, status_name: str, summary: str = ""):
-        self.fields = MockedFields(components, status_name, summary)
+FakeComponent = namedtuple("FakeComponent", ["name"])
+FakeStatus = namedtuple("FakeStatus", ["name"])
+FakeFields = namedtuple("FakeFields", ["components", "status", "summary"])
+FakeIssue = namedtuple("FakeIssue", ["fields", "key"])
+FakeKeyLessIssue = namedtuple("FakeKeyLessIssue", ["fields"])
 
 
 def test_when_empty_issue_provided():
@@ -52,31 +19,50 @@ def test_when_empty_issue_provided():
 
 @pytest.mark.parametrize("issue_key", ["", None])
 def test_when_issue_has_no_key(issue_key):
-    issue = MockedIssue(issue_key, ["c1"], "Selected For Release")
+    issue = FakeIssue(
+        FakeFields(
+            [FakeComponent("c1")], FakeStatus("Selected For Release"), ""
+        ),
+        issue_key,
+    )
     with pytest.raises(ValueError):
         parse_jira_issue(issue)
 
 
 def test_when_issue_has_no_key_attr():
-    issue = MockedKeylessIssue(["c1"], "invalid status")
+    issue = FakeKeyLessIssue(
+        FakeFields([FakeComponent("c1")], FakeStatus("invalid status"), "")
+    )
     with pytest.raises(ValueError):
         parse_jira_issue(issue)
 
 
 def test_when_issue_has_no_component():
-    issue = MockedIssue("issue key", [], "Selected For Release")
+    issue = FakeIssue(
+        FakeFields([], FakeStatus("Selected For Release"), ""), "issue key"
+    )
     with pytest.raises(ValueError):
         parse_jira_issue(issue)
 
 
 def test_when_issue_has_more_than_one_component():
-    issue = MockedIssue("issue key", ["c1", "c2"], "Selected For Release")
+    issue = FakeIssue(
+        FakeFields(
+            [FakeComponent("c1"), FakeComponent("c2")],
+            FakeStatus("Selected For Release"),
+            "",
+        ),
+        "issue key",
+    )
     with pytest.raises(ValueError):
         parse_jira_issue(issue)
 
 
 def test_when_issue_has_invalid_status():
-    issue = MockedIssue("issue key", ["c1"], "invalid status")
+    issue = FakeIssue(
+        FakeFields([FakeComponent("c1")], FakeStatus("invalid status"), ""),
+        "issue key",
+    )
     with pytest.raises(ValueError):
         parse_jira_issue(issue)
 
@@ -85,7 +71,10 @@ def test_when_issue_has_invalid_status():
     "issue_key,issue_status", [("issue key", "Selected For Release")]
 )
 def test_when_issue_is_valid(issue_key, issue_status):
-    issue = MockedIssue(issue_key, ["c1"], issue_status)
+    issue = FakeIssue(
+        FakeFields([FakeComponent("c1")], FakeStatus(issue_status), ""),
+        issue_key,
+    )
     nova_task = parse_jira_issue(issue)
     assert nova_task is not None
 
@@ -104,12 +93,21 @@ def test_when_issue_is_valid(issue_key, issue_status):
 def test_if_jira_issue_belongs_to_component(
     jira_cmp_name, nova_cmp_name, expected
 ):
-    issue = MockedIssue("issue key", [jira_cmp_name], "Selected For Release")
+    issue = FakeIssue(
+        FakeFields(
+            [FakeComponent(jira_cmp_name)],
+            FakeStatus("Selected For Release"),
+            "",
+        ),
+        "issue key",
+    )
     result = filter_jira_issue(issue, nova_cmp_name)
     assert result == expected
 
 
 def test_when_jira_issue_has_no_component():
-    issue = MockedIssue("issue key", [], "Selected For Release")
+    issue = FakeIssue(
+        FakeFields([], FakeStatus("Selected For Release"), ""), "issue key"
+    )
     with pytest.raises(ValueError):
         filter_jira_issue(issue, "c1")
