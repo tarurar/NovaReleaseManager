676f8647316c79dcd5fca4c3c4fce3d742b35852
atarutin <andrey.tarutin@lykke-business.ch>
2023-08-23 17:36:00 +0300

    remove pylint from requirements

--- csv_utils.py
+++ csv_utils.py
@@ -12,9 +12,9 @@ def export_packages_to_csv(data, file_name: str = DEFAULT_CSV_OUTPUT):
     if file_name is None:
         file_name = DEFAULT_CSV_OUTPUT
 
-    with open(file_name, "w", newline="", encoding="utf-8") as f:
+    with open(file_name, "w", newline="", encoding="utf-8") as csv_file:
         fieldnames = ["package", "tag", "date", "url"]
-        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
         for row in data:
             writer.writerow(row)
