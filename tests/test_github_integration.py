f26275bb289c62a477fefcd9001b51d78004a925
atarutin <andrey.tarutin@lykke-business.ch>
2023-08-25 00:07:07 +0300

    add FakeConfig and more unit tests to cover github integration

--- tests/test_github_integration.py
+++ tests/test_github_integration.py
@@ -10,14 +10,32 @@ from core.nova_release import NovaRelease
 from integration.github import GitHubIntegration
 
 
+# region Fake classes
+
+
+class FakeConfig:
+    """Configuration object for fakes"""
+
+    def __init__(self, tags_count: int = 5, create_release: bool = True):
+        """
+        :param tags_count: number of fake tags to create
+        :param create_release: whether to create a fake release
+        """
+        if tags_count < 1:
+            raise ValueError("Tags count must be greater than zero")
+
+        self.tags_count = tags_count
+        self.create_release = create_release
+
+
 class FakeGitHub:
     """Fake GitHub API client."""
 
-    def __init__(self):
-        pass
+    def __init__(self, config: FakeConfig):
+        self.__config = config
 
     def get_repo(self, _):
-        return FakeRepository()
+        return FakeRepository(self.__config)
 
 
 class FakeGitTag:
@@ -83,21 +101,20 @@ class FakeRepository:
     Initial tags are created in the range from 1 to 10.
     """
 
-    def __init__(self):
-        self.__tags = list(
-            map(
-                lambda x: FakeTag(
-                    str(x),
-                    FakeCommit(
-                        FakeGitCommit(
-                            date.today().strftime("%Y-%m-%d"),
-                            FakeGitAuthor(f"Author {x}"),
-                        )
-                    ),
+    def __init__(self, config: FakeConfig):
+        self.__config = config
+        self.__tags = [
+            FakeTag(
+                str(i),
+                FakeCommit(
+                    FakeGitCommit(
+                        date.today().strftime("%Y-%m-%d"),
+                        FakeGitAuthor(f"Author {i}"),
+                    )
                 ),
-                range(1, 11),
             )
-        )
+            for i in range(1, self.__config.tags_count + 1)
+        ]
 
     def get_tags(self) -> list[FakeTag]:
         return self.__tags
@@ -116,10 +133,18 @@ class FakeRepository:
     def create_git_ref(self, ref, sha):
         pass
 
-    def create_git_release(self, tag, name, message) -> FakeGitRelease:
-        return FakeGitRelease(tag, name, message)
+    def create_git_release(self, tag, name, message) -> FakeGitRelease | None:
+        return (
+            FakeGitRelease(tag, name, message)
+            if self.__config.create_release
+            else None
+        )
 
 
+# endregion
+
+
+# region Fixtures
 @pytest.fixture(name="fake_input_new_tag_values")
 def fixture_fake_input_new_tag_values():
     """
@@ -179,14 +204,42 @@ def fixture_fake_input_second_tag_cancel_func(
     return fake_input_second_tag_cancel
 
 
+@pytest.fixture(name="fake_input_both_tags_values")
+def fixture_fake_input_both_tags_values():
+    """
+    The sequence of inputs emulates the following user actions:
+    1. Select the first tag from the list of existing tags.
+    2. Select the second tag from the list of existing tags.
+    """
+    inputs = ["1", "2"]
+    return iter(inputs)
+
+
+@pytest.fixture(name="fake_input_both_tags_func")
+def fixture_fake_input_both_tags_func(fake_input_both_tags_values):
+    def fake_input_both_tags(_):
+        return next(fake_input_both_tags_values)
+
+    return fake_input_both_tags
+
+
+@pytest.fixture(name="fake_config")
+def fixture_fake_config(request):
+    # use default FakeConfig if no param is provided
+    return request.param if hasattr(request, "param") else FakeConfig()
+
+
 @pytest.fixture(name="fake_github")
-def fixture_fake_github():
-    return FakeGitHub()
+def fixture_fake_github(fake_config):
+    return FakeGitHub(fake_config)
 
 
 @pytest.fixture(name="integration")
 def fixture_integration(fake_github):
     return GitHubIntegration(fake_github)
+
+
+# endregion
 
 
 def test_get_repository_top_tags_default(
@@ -198,7 +251,7 @@ def test_get_repository_top_tags_default(
     assert len(tags) == default_tags_count
 
 
-@pytest.mark.parametrize("tags_count", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+@pytest.mark.parametrize("tags_count", [1, 2, 3, 4, 5])
 def test_get_repository_top_tags(integration: GitHubIntegration, tags_count):
     tags = integration.get_repository_top_tags("repo_url", tags_count)
     assert len(tags) == tags_count
@@ -285,3 +338,47 @@ def test_create_git_release_if_tag_not_selected_returns_none(
     )
 
     assert release is None
+
+
+@pytest.mark.parametrize("fake_config", [FakeConfig(1)], indirect=True)
+def test_create_git_release_if_previous_tag_not_selected_returns_none(
+    monkeypatch,
+    integration: GitHubIntegration,
+    fake_input_second_tag_cancel_func,
+):
+    """
+    Here the original FakeConfig fixture is replaced with the one that provides
+    only one tag. This is done to emulate the situation when there is only one
+    tag in the repository and the user cancels the selection of the second tag.
+    Autoselection of the second tag is not possible in this case. So, the
+    create_git_release() method should return None.
+    """
+    monkeypatch.setattr("builtins.input", fake_input_second_tag_cancel_func)
+    release = integration.create_git_release(
+        NovaRelease("project", "version", "delivery"),
+        NovaComponent("component", CodeRepository(GitCloudService.GITHUB, "")),
+    )
+
+    assert release is None
+
+
+@pytest.mark.parametrize(
+    "fake_config", [FakeConfig(create_release=False)], indirect=True
+)
+def test_create_git_release_raises_exception_if_release_not_created(
+    monkeypatch, integration: GitHubIntegration, fake_input_both_tags_func
+):
+    """
+    Here the original FakeConfig fixture is replaced with the one that
+    prevents the creation of the release. This is done to emulate the situation
+    when the user selects two tags and the release is not created for some
+    reason. The create_git_release() method should raise an exception.
+    """
+    monkeypatch.setattr("builtins.input", fake_input_both_tags_func)
+    with pytest.raises(IOError):
+        integration.create_git_release(
+            NovaRelease("project", "version", "delivery"),
+            NovaComponent(
+                "component", CodeRepository(GitCloudService.GITHUB, "")
+            ),
+        )
