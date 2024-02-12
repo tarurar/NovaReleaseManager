681111615bdeb835de073f10467a4668bf18c14f
atarutin <andrey.tarutin@lykke-business.ch>
2024-02-12 16:10:17 +0300

    Commit usual tag together with "nova-" Fixes #64

--- workers/bitbucket_worker.py
+++ workers/bitbucket_worker.py
@@ -89,9 +89,12 @@ class BitbucketReleaseWorker(ReleaseWorker):
             )
             msbuild.update_solution_version(sources_dir, new_version)
 
-            tag_name = f"nova-{str(new_version)}"
+            tag_name = f"v{str(new_version)}"
             self.__gi.commit_changelogs_and_csproj(
                 sources_dir, f"Version {str(new_version)}"
+            )
+            self.__gi.tag(
+                sources_dir, f"nova-{str(new_version)}", self._release.title
             )
             self.__gi.tag(sources_dir, tag_name, self._release.title)
 
