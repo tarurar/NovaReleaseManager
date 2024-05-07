a5029305a8de505e0c26a1a326437ee35b2d9e31
atarutin <andrey.tarutin@lykke-business.ch>
2024-05-07 18:24:40 +0300

    use markdown extension to properly format code blocks Fixes #71

--- fs_utils.py
+++ fs_utils.py
@@ -216,7 +216,9 @@ def markdown_to_pdf(markdown_file_path: str, pdf_file_path: str):
 
     with open(markdown_file_path, "r", encoding="utf-8") as file_handle:
         markdown_content = file_handle.read()
-    html_content = markdown.markdown(markdown_content)
+    html_content = markdown.markdown(
+        markdown_content, extensions=["fenced_code"]
+    )
     pdfkit.from_string(html_content, pdf_file_path)
 
 
